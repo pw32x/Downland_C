4d8da49976c02177f3cd792cbdc8387855863be6
pw_32x <git@puffweet.com>
2025-02-04 16:36:00 -0500

    improved the CRT artifact effect to match trs80gp's

--- main.c
+++ main.c
@@ -10,11 +10,10 @@ static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
 #define FB_WIDTH  256
-#define HALF_FB_WIDTH (FB_WIDTH / 2)
 #define FB_HEIGHT 192
 #define FB_PITCH  (FB_WIDTH / 8)
 
-#define SCREEN_SCALE 2
+#define SCREEN_SCALE 3
 #define SCREEN_WIDTH (FB_WIDTH * SCREEN_SCALE)
 #define SCREEN_HEIGHT (FB_HEIGHT * SCREEN_SCALE)
 
@@ -22,8 +21,8 @@ Uint8 framebuffer[FB_HEIGHT * FB_PITCH]; // 1-bit framebuffer
 SDL_Texture* framebufferTexture = NULL;
 
 // half frame buffer where we will generate CRT artifact effects
-uint32_t halfFramebuffer[HALF_FB_WIDTH * FB_HEIGHT];
-SDL_Texture* halfFramebufferTexture = NULL;
+uint32_t crtFramebuffer[FB_WIDTH * FB_HEIGHT];
+SDL_Texture* crtFramebufferTexture = NULL;
 
 // Set or clear a pixel in the 1-bit framebuffer
 void set_pixel(int x, int y, int value) 
@@ -64,46 +63,81 @@ void updateFramebufferTexture(SDL_Texture* framebufferTexture)
 }
 
 
-void updateHalfFrameBufferAndTexture(SDL_Renderer* renderer) 
+void updateCrtFramebufferAndTexture(SDL_Renderer* renderer) 
 {
-    // Iterate over the source buffer and fill the texture data
+    // Color definitions
+    const uint32_t BLACK  = 0x000000; // 00 black
+    const uint32_t BLUE   = 0x0000FF; // 01 blue
+    const uint32_t ORANGE = 0xFFA500; // 10 orange
+    const uint32_t WHITE  = 0xFFFFFF; // 11 white
+
     for (int y = 0; y < FB_HEIGHT; ++y) 
     {
-        for (int x = 0; x < HALF_FB_WIDTH; ++x) 
+        // every pair of bits generates a color for the two corresponding
+        // pixels of the destination texture, so:
+        // source bits:        00 01 10 11
+        // final pixel colors: black, black, blue, blue, orange, orange, white, white.
+
+        for (int x = 0; x < FB_WIDTH; x += 2) 
         {
-            // Calculate the corresponding 1bpp source pixel
-            int srcX = x * 2;
-            int srcY = y;
+            int byteIndex = (y * 32) + (x / 8);
+            int bitOffset = 7 - (x % 8);
 
-            uint8_t byte = framebuffer[srcY * 32 + (srcX / 8)];
-            uint8_t bit1 = (byte >> (7 - (srcX % 8))) & 1;
-            uint8_t bit2 = (byte >> (6 - (srcX % 8))) & 1;
+            // Read two adjacent bits
+            uint8_t bit1 = (framebuffer[byteIndex] >> bitOffset) & 1;
+            uint8_t bit2 = (framebuffer[byteIndex] >> (bitOffset - 1)) & 1;
 
-            uint32_t color = 0;
-            if (bit1 == 0 && bit2 == 0) 
+            // Determine base color
+            uint32_t color = BLACK;
+            if (bit1 == 0 && bit2 == 1) color = BLUE;
+            else if (bit1 == 1 && bit2 == 0) color = ORANGE;
+            else if (bit1 == 1 && bit2 == 1) color = WHITE;
+
+            // Apply base color
+            crtFramebuffer[y * FB_WIDTH + x]     = color;
+            crtFramebuffer[y * FB_WIDTH + x + 1] = color;
+        }
+
+        // Apply a quick and dirty crt artifact effect
+        // pixels whose original bits are adjacent are converted to white
+        // source colors: black, black, blue, blue, orange, orange, white, white.
+        // source bits:  00 01 10 11
+        // seen as:      00 00 01 01 10 10 11 11 // forth and fifth pairs have adjacent bits. 
+        //                                          Turn both corresponding pixels to white.
+        //                                          Also turn off the other pixel in the pair to
+        //                                          black.
+        // final final:  black, black, black, white, white, black, white, white
+        for (int x = 0; x < FB_WIDTH; x += 2) 
+        {
+            uint32_t pixel1 = crtFramebuffer[y * FB_WIDTH + x];
+            uint32_t pixel2 = crtFramebuffer[y * FB_WIDTH + x + 1];
+
+            if (pixel2 == BLUE && x < FB_WIDTH - 2)
             {
-                color = 0x000000; // Black
-            } 
-            else if (bit1 == 0 && bit2 == 1) 
+                uint32_t pixel3 = crtFramebuffer[y * FB_WIDTH + x + 2];
+                if (pixel3 == ORANGE || pixel3 == WHITE)
+                {
+                    pixel2 = WHITE;
+                    pixel1 = BLACK;
+                }
+            }
+            else if (pixel1 == ORANGE && x > 2)
             {
-                color = 0x0000FF; // Blue
-            } 
-            else if (bit1 == 1 && bit2 == 0) 
-            {
-                color = 0xFFA500; // Orange
-            } 
-            else if (bit1 == 1 && bit2 == 1) 
-            {
-                color = 0xFFFFFF; // White
+                uint32_t pixel0 = crtFramebuffer[y * FB_WIDTH + x - 1];
+                if (pixel0 == BLUE || pixel0 == WHITE)
+                {
+                    pixel1 = WHITE;
+                    pixel2 = BLACK;
+                }
             }
 
-            // Set the pixel color in the texture data array
-            halfFramebuffer[y * HALF_FB_WIDTH + x] = color;
+            crtFramebuffer[y * FB_WIDTH + x] = pixel1;
+            crtFramebuffer[y * FB_WIDTH + x + 1] = pixel2;
         }
     }
 
     // Update the texture with the new data
-    SDL_UpdateTexture(halfFramebufferTexture, NULL, halfFramebuffer, HALF_FB_WIDTH * sizeof(uint32_t));
+    SDL_UpdateTexture(crtFramebufferTexture, NULL, crtFramebuffer, FB_WIDTH * sizeof(uint32_t));
 }
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
@@ -134,12 +168,12 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     SDL_SetTextureScaleMode(framebufferTexture, SDL_SCALEMODE_NEAREST); // no smoothing
 
     // Create the texture
-    halfFramebufferTexture = SDL_CreateTexture(renderer, 
+    crtFramebufferTexture = SDL_CreateTexture(renderer, 
                                                SDL_PIXELFORMAT_XRGB8888, 
                                                SDL_TEXTUREACCESS_STREAMING, 
-                                               HALF_FB_WIDTH, 
+                                               FB_WIDTH, 
                                                FB_HEIGHT);
-    SDL_SetTextureScaleMode(halfFramebufferTexture, SDL_SCALEMODE_NEAREST); // no smoothing
+    SDL_SetTextureScaleMode(crtFramebufferTexture, SDL_SCALEMODE_NEAREST); // no smoothing
 
     srand((unsigned int)time(NULL));
 
@@ -179,9 +213,9 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     updateFramebufferTexture(framebufferTexture); 
     //SDL_RenderTexture(renderer, framebufferTexture, NULL, NULL); // Scale to window
 
-    // Update texture from halfFramebuffer
-    updateHalfFrameBufferAndTexture(renderer);
-    SDL_RenderTexture(renderer, halfFramebufferTexture, NULL, NULL); // Scale to window
+    // Update texture from crtFramebuffer
+    updateCrtFramebufferAndTexture(renderer);
+    SDL_RenderTexture(renderer, crtFramebufferTexture, NULL, NULL); // Scale to window
 
     /*
     // write debug text

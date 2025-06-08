acac2092c9a214151e35dcd5327655dabffa4fe2
pw_32x <git@puffweet.com>
2025-06-07 20:56:50 -0400

    clean up of test code in main.cpp

--- platforms/saturn/source/main.cpp
+++ platforms/saturn/source/main.cpp
@@ -9,16 +9,6 @@ extern "C"
 
 #include "game_runner.hpp"
 
-
-extern uint16_t VDP2_CYCA0L;
-extern uint16_t VDP2_CYCA0U;
-extern uint16_t VDP2_CYCA1L;
-extern uint16_t VDP2_CYCA1U;
-extern uint16_t VDP2_CYCB0L;
-extern uint16_t VDP2_CYCB0U;
-extern uint16_t VDP2_CYCB1L;
-extern uint16_t VDP2_CYCB1U;
-
 using namespace SRL::Types;
 using namespace SRL::Input;
 using namespace SRL::Math;
@@ -42,65 +32,6 @@ void Sound_Stop(u8 soundindex)
     //soundManager.stop(soundindex);
 }
 }
-
-
-class GameObject
-{
-public:
-    int16_t m_textureIndex;
-    int16_t m_x;
-    int16_t m_y;
-    int16_t m_width;
-    int16_t m_height;
-
-public:
-    GameObject(int16_t x, 
-               int16_t y, 
-               SRL::Bitmap::IBitmap* bitmap) 
-        : m_textureIndex(-1), 
-          m_x(x), 
-          m_y(y),
-          m_width(0),
-          m_height(0)
-    {
-        m_textureIndex = SRL::VDP1::TryLoadTexture(bitmap, PaletteUtils::PaletteLoader::Load);
-
-        m_width = bitmap->GetInfo().Width;
-        m_height = bitmap->GetInfo().Height;
-    }
-
-    GameObject(int16_t _x, int16_t _y, const char* filename) 
-        : m_textureIndex(-1), 
-          m_x(_x), 
-          m_y(_y),
-          m_width(0),
-          m_height(0)
-    {
-        // Load texture
-        SRL::Bitmap::TGA *tga = new SRL::Bitmap::TGA(filename); // Loads TGA file into main RAM
-
-        m_textureIndex = SRL::VDP1::TryLoadTexture(tga, PaletteUtils::PaletteLoader::Load);    // Loads TGA into VDP1
-
-        m_width = tga->GetInfo().Width;
-        m_height = tga->GetInfo().Height;
-
-        delete tga;
-    }
-
-    void Draw()
-    {
-        static SRL::Math::Types::Vector2D points[4];
-
-        GeometryHelpers::Quad::setup(m_x, 
-                                     m_y, 
-                                     m_width, 
-                                     m_height, 
-                                     points);
-
-        // Simple sprite
-        SRL::Scene2D::DrawSprite(m_textureIndex, points, 500);
-    }
-};
 
 
 void Update_Controls(int controllerIndex, 
@@ -168,60 +99,6 @@ int main()
 
     g_gameRunner = new GameRunner(g_resources);
 
-    u8 customSprite[16*16] = 
-    {
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-    };
-
-    u8 playerObjectBuffer[16*16];
-    ImageUtils::ImageConverter::convert1bppImageTo8bppCrtEffectImage(g_resources->sprites_player,
-                                                                     playerObjectBuffer,
-                                                                     16,
-                                                                     16,
-                                                                     ImageUtils::ImageConverter::CrtColor::Blue);
-
-
-
-    BitmapUtils::InMemoryBitmap myBitmap(customSprite, 
-                                         16, 
-                                         16, 
-                                         PaletteUtils::g_downlandPalette, 
-                                         PaletteUtils::g_downlandPaletteColorsCount);
-
-    BitmapUtils::InMemoryBitmap myBitmap2(playerObjectBuffer, 
-                                          16, 
-                                          16, 
-                                          PaletteUtils::g_downlandPalette, 
-                                          PaletteUtils::g_downlandPaletteColorsCount);
-
-    GameObject myCustomObject(160, 40, &myBitmap);
-    GameObject playerObject(220, 40, &myBitmap2);
-
-    GameObject myObject1(0, 40, "TEST.TGA");
-    GameObject myObject2(80, 120, "TEST8BPP.TGA");
-    GameObject myObject3(160, 160, "TEST4BPP.TGA");
-
-
-
-    //SRL::Tilemap::Interfaces::CubeTile* TestTilebin = new SRL::Tilemap::Interfaces::CubeTile("SPACE.BIN");//Load tilemap from cd to work RAM
-    //SRL::VDP2::NBG0::LoadTilemap(*TestTilebin);//Transfer tilemap from work RAM to VDP2 VRAM and register with NBG0
-    //delete TestTilebin;//free work RAM
-
     u8* frameBuffer8bpp = new u8[FRAMEBUFFER_WIDTH * FRAMEBUFFER_HEIGHT];
 
     ImageUtils::ImageConverter::convert1bppImageTo8bppCrtEffectImage(g_gameRunner->m_gameData.cleanBackground,
@@ -230,28 +107,15 @@ int main()
                                                                      FRAMEBUFFER_HEIGHT,
                                                                      ImageUtils::ImageConverter::CrtColor::Blue);
 
-    //for (int loop = 0; loop < FRAMEBUFFER_WIDTH * FRAMEBUFFER_HEIGHT; loop++)
-    //    frameBuffer8bpp[loop] = 0;
-    //
-    //for (int loop = 0; loop < SCREEN_WIDTH; loop++)
-    //    frameBuffer8bpp[loop] = loop % 4;
-
-
     BitmapUtils::InMemoryBitmap* framebufferBitmap = new BitmapUtils::InMemoryBitmap(frameBuffer8bpp, 
                                                                                      FRAMEBUFFER_WIDTH, 
                                                                                      FRAMEBUFFER_HEIGHT, 
                                                                                      PaletteUtils::g_downlandPalette, 
                                                                                      PaletteUtils::g_downlandPaletteColorsCount);
 
-    SRL::Bitmap::TGA* logo = new SRL::Bitmap::TGA("LOGO1.TGA");//Load Bitmap image to work RAM
-    SRL::Tilemap::Interfaces::Bmp2Tile* TestTilebmp = new SRL::Tilemap::Interfaces::Bmp2Tile(*logo);//convert bitmap to tilemap
-
     SRL::Tilemap::Interfaces::Bmp2Tile* tileSet = new SRL::Tilemap::Interfaces::Bmp2Tile(*framebufferBitmap);
-    //SRL::VDP2::NBG0::LoadTilemap(*TestTilebmp);//Transfer tilemap from work RAM to VDP2 VRAM and register with NBG0
     SRL::VDP2::NBG0::LoadTilemap(*tileSet);
     
-
-    delete TestTilebmp;//free tilemap from work ram 
     delete tileSet;//free work RAM
     delete [] frameBuffer8bpp;
     delete framebufferBitmap;
@@ -334,21 +198,9 @@ int main()
 
         SRL::Core::Synchronize();
 
-        //const Drop* drop = &g_gameRunner->m_gameData.dropData.drops[0];
-
         //SRL::Debug::Print(1,17,"x %02d", drop->x << 1);
 	    //SRL::Debug::Print(1,18,"y %02d", drop->y >> 8);
-
-        //myObject1.m_x = drop->x << 1;
-        //myObject1.m_y = drop->y >> 8;
-
         g_gameRunner->draw();
-
-        //myObject1.Draw();
-        //myObject2.Draw();
-        //myObject3.Draw();
-        //myCustomObject.Draw();
-        //playerObject.Draw();
 
         //move positions of NBG0 and NBG1 scrolls:
         //Nbg0Position += Vector2D(1.0, 1.0);

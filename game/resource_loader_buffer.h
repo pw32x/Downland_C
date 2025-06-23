d81a672d74716974efeaeed891ba84b5668f458b
pw_32x <git@puffweet.com>
2025-06-23 16:02:59 -0400

    removed unneeded defines

--- game/resource_loader_buffer.h
+++ game/resource_loader_buffer.h
@@ -4,11 +4,6 @@
 #include "..\game\base_types.h"
 #include "..\game\resource_types.h"
 
-#define RESLOAD_BUFFER_RESULT_OK				0
-#define RESLOAD_BUFFER_RESULT_CHECKSUMFAILED	1
-#define RESLOAD_BUFFER_RESULT_WRONGROMSIZE		2
-#define RESLOAD_BUFFER_RESULT_UNKNOWNFAILURE	0xff
-
 int ResourceLoaderBuffer_Init(const u8* fileBuffer, u32 fileBufferSize, Resources* resources);
 void ResourceLoaderBuffer_Shutdown(Resources* resources);
 

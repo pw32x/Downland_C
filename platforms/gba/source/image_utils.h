60281cb3262dbcf83db34293867bca6a57a30f1c
pw_32x <git@puffweet.com>
2025-06-24 10:40:18 -0400

    moved image conversion functions to image_utils.*

---
+++ platforms/gba/source/image_utils.h
@@ -0,0 +1,23 @@
+#ifndef IMAGE_UTILS_INCLUDE_H
+#define IMAGE_UTILS_INCLUDE_H
+
+#include "..\..\..\game\base_types.h"
+
+enum CrtColor
+{
+    CrtColor_Blue,
+    CrtColor_Orange
+};
+
+void convert1bppImageTo8bppCrtEffectImage(const dl_u8* originalImage,
+                                          dl_u8* destinationImage,
+                                          dl_u16 width,
+                                          dl_u16 height,
+                                          enum CrtColor crtColor);
+
+void convertToTiles(const dl_u8* sprite, 
+					dl_u8 width,
+					dl_u8 height,
+					int tileIndexStartInBytes);
+
+#endif
--- platforms/gba/source/main.c
+++ platforms/gba/source/main.c
@@ -24,6 +24,7 @@
 
 #include "r6502_portfont_bin.h"
 #include "downland_rom.h"
+#include "image_utils.h"
 
 #include "..\..\..\game\base_types.h"
 #include "..\..\..\game\resource_types.h"
@@ -57,127 +58,6 @@ void Sound_Play(dl_u8 soundIndex, dl_u8 loop)
 
 void Sound_Stop(dl_u8 soundIndex)
 {
-}
-
-enum CrtColor
-{
-    CrtColor_Blue,
-    CrtColor_Orange
-};
-
-void convert1bppImageTo8bppCrtEffectImage(const dl_u8* originalImage,
-                                          dl_u8* destinationImage,
-                                          dl_u16 width,
-                                          dl_u16 height,
-                                          enum CrtColor crtColor) 
-{
-    const dl_u8 bytesPerRow = width / 8;
-
-    // Color definitions
-    const dl_u8 BLACK  = 0x00; // 00 black
-    const dl_u8 BLUE   = crtColor == CrtColor_Blue ? 0x1 : 0x2; // 01 blue
-    const dl_u8 ORANGE = crtColor == CrtColor_Blue ? 0x2 : 0x1; // 10 orange
-    const dl_u8 WHITE  = 0x3; // 11 white
-
-    for (int y = 0; y < height; ++y) 
-    {
-        // every pair of bits generates a color for the two corresponding
-        // pixels of the destination texture, so:
-        // source bits:        00 01 10 11
-        // final pixel colors: black, black, blue, blue, orange, orange, white, white.
-        dl_u32 yOffset = y * width;
-
-        for (int x = 0; x < width; x += 2) 
-        {
-            int byteIndex = (y * bytesPerRow) + (x / 8);
-            int bitOffset = 7 - (x % 8);
-
-            // Read two adjacent bits
-            dl_u8 bit1 = (originalImage[byteIndex] >> bitOffset) & 1;
-            dl_u8 bit2 = (originalImage[byteIndex] >> (bitOffset - 1)) & 1;
-
-            // Determine base color
-            dl_u8 color = BLACK;
-            if (bit1 == 0 && bit2 == 1) color = BLUE;
-            else if (bit1 == 1 && bit2 == 0) color = ORANGE;
-            else if (bit1 == 1 && bit2 == 1) color = WHITE;
-
-            // Apply base color
-            destinationImage[yOffset + x]     = color;
-            destinationImage[yOffset + x + 1] = color;
-        }
-
-        // Apply a quick and dirty crt artifact effect
-        // pixels whose original bits are adjacent are converted to white
-        // source colors: black, black, blue, blue, orange, orange, white, white.
-        // source bits:  00 01 10 11
-        // seen as:      00 00 01 01 10 10 11 11 // forth and fifth pairs have adjacent bits. 
-        //                                          Turn both corresponding pixels to white.
-        //                                          Also turn off the other pixel in the pair to
-        //                                          black.
-        // final final:  black, black, black, white, white, black, white, white
-        for (int x = 0; x < width; x += 2) 
-        {
-            dl_u8 leftPixel = destinationImage[yOffset + x];
-            dl_u8 rightPixel = destinationImage[yOffset + x + 1];
-
-            if (rightPixel == BLUE && x < width - 2)
-            {
-                dl_u8 pixel3 = destinationImage[yOffset + x + 2];
-                if (pixel3 == ORANGE || pixel3 == WHITE)
-                {
-                    rightPixel = WHITE;
-                    leftPixel = BLACK;
-                }
-            }
-            else if (leftPixel == ORANGE && x >= 2)
-            {
-                dl_u8 pixel0 = destinationImage[yOffset + x - 1];
-                if (pixel0 == BLUE || pixel0 == WHITE)
-                {
-                    leftPixel = WHITE;
-                    rightPixel = BLACK;
-                }
-            }
-
-            destinationImage[yOffset + x] = leftPixel;
-            destinationImage[yOffset + x + 1] = rightPixel;
-        }
-    }
-}
-
-
-void convertToTiles(const dl_u8* sprite, 
-					dl_u8 width,
-					dl_u8 height,
-					int tileIndexStartInBytes)
-{
-	dl_u8 tileWidth = width / 8;
-	dl_u8 tileHeight = height / 8;
-
-	// vram only accepts 16bit writes
-	dl_u16* vramRunner = (dl_u16*)(CHAR_BASE_BLOCK(4) + tileIndexStartInBytes);
-
-	for (int tiley = 0; tiley < tileHeight; tiley++)
-	{
-		int starty = tiley * 8;
-
-		for (int tilex = 0; tilex < tileWidth; tilex++)
-		{
-			int startx = tilex * 8;
-
-			for (int loopy = 0; loopy < 8; loopy++)
-			{
-				for (int loopx = 0; loopx < 8; loopx += 2)
-				{
-					// write two pixels at a time
-					int pixelIndex = (startx + loopx) + ((starty + loopy) * width);
-					*vramRunner = sprite[pixelIndex] | (sprite[pixelIndex + 1] << 8);
-					vramRunner++;
-				}
-			}
-		}
-	}
 }
 
 // --------------------------------------------------------------------

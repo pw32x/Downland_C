538fecd1cd131a45ac0bc32a068d20a4a0e14078
pw_32x <git@puffweet.com>
2025-08-17 12:58:05 -0400

    remove DLExporter from build for now

--- platforms/genesis/build/build.bat
+++ platforms/genesis/build/build.bat
@@ -1,6 +1,6 @@
 pushd ..
 SET "GDK_WIN=..\..\..\..\..\GenDev\SGDK"
 SET "GDK=../../../../../GenDev/SGDK"
-tools\bin\DLExporter.exe
+REM tools\bin\DLExporter.exe
 %GDK_WIN%\bin\make -f build/makefile.gen
 popd

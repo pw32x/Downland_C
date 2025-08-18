40c9beee07ebfd038ae59bfdecb04f9070095488
pw_32x <git@puffweet.com>
2025-08-17 21:26:04 -0400

    draw titlescreen & chamber + load and display of sprites

--- platforms/genesis/source/game_runner.c
+++ platforms/genesis/source/game_runner.c
@@ -1,6 +1,15 @@
 #include "game_runner.h"
 
 #include <genesis.h>
+#include "kdebug.h"
+
+#include "tilemaps.h"
+#define TILE_MAP_WIDTH (FRAMEBUFFER_WIDTH / 8)
+#define TILE_MAP_HEIGHT (FRAMEBUFFER_HEIGHT / 8)
+#define TILE_MAP_SIZE (TILE_MAP_WIDTH * TILE_MAP_HEIGHT)
+
+// generated headers
+#include "tileset.h"
 
 /*
 #include <gba_sprites.h>
@@ -16,9 +25,10 @@
 #include "transition_effect.h"
 */
 
-#include "..\..\..\game\drops_manager.h"
-#include "..\..\..\game\draw_utils.h"
-#include "..\..\..\game\rooms\chambers.h"
+#include "drops_manager.h"
+#include "draw_utils.h"
+#include "rooms\chambers.h"
+#include "rooms\titlescreen.h"
 
 /*
 typedef struct
@@ -50,11 +60,12 @@ dl_u16 g_oldPlayerY = 0;
 typedef struct
 {
 	//const SpriteAttributes* spriteAttributes;
-	dl_u16 tileIndex;
+	const TileSet* tileSet;
+	dl_u16 vdpTileIndex;
 	dl_u8 tilesPerFrame;
+	dl_u8 vdpSize;
 } GameSprite;
 
-/*
 GameSprite playerSprite;
 GameSprite dropsSprite;
 GameSprite cursorSprite;
@@ -66,13 +77,12 @@ GameSprite moneyBagSprite;
 GameSprite doorSprite;
 GameSprite regenSprite;
 GameSprite splatSprite;
-GameSprite characterFont;
-GameSprite hudCharacterFont;
+GameSprite characterFontSprite;
+GameSprite hudCharacterFontSprite;
 GameSprite playerIconSprite;
 GameSprite playerIconSpriteRegen;
 
 const GameSprite* g_pickUpSprites[3];
-*/
 
 typedef void (*DrawRoomFunction)(struct GameData* gameData, const Resources* resources);
 DrawRoomFunction m_drawRoomFunctions[NUM_ROOMS_AND_ALL];
@@ -89,6 +99,55 @@ void drawGetReadyScreen(struct GameData* gameData, const Resources* resources);
 // scrolling
 
 //m_characterFont(resources->characterFont, 8, 7, 39),
+
+typedef void (*RoomDrawFunction)(dl_u8 roomNumber, struct GameData* gameData, const Resources* resources);
+RoomDrawFunction g_originalRoomDrawFunctions[NUM_ROOMS_AND_ALL];
+
+dl_u16 g_numSpritesToDraw = 0;
+
+dl_u16 g_vdpTileIndex = 0;
+void buildTileResource(GameSprite* sprite, 
+					   const TileSet* tileSet, 
+					   dl_u8 spriteWidth, 
+					   dl_u8 spriteHeight,
+					   dl_u8 numFrames,
+					   dl_u8 vdpSize)
+{
+	dl_u16 numTiles = ((spriteWidth + 7) / 8) * ((spriteHeight + 7) / 8);
+
+	sprite->tileSet = tileSet;
+	sprite->vdpTileIndex = g_vdpTileIndex;
+	sprite->tilesPerFrame = numTiles;
+	sprite->vdpSize = vdpSize;
+
+	VDP_loadTileSet(tileSet, g_vdpTileIndex, DMA);
+
+	g_vdpTileIndex += (numTiles * numFrames);
+}
+
+void buildSpriteResource(GameSprite* sprite, 
+					     const SpriteDefinition* spriteDef, 
+					     dl_u8 spriteWidth, 
+					     dl_u8 spriteHeight,
+					     dl_u8 numFrames,
+					     dl_u8 vdpSize)
+{
+	dl_u16 numTiles = ((spriteWidth + 7) / 8) * ((spriteHeight + 7) / 8);
+
+	sprite->tileSet = spriteDef->animations[0]->frames[0]->tileset;
+	sprite->vdpTileIndex = g_vdpTileIndex;
+	sprite->tilesPerFrame = numTiles;
+	sprite->vdpSize = vdpSize;
+
+	for (int loop = 0; loop < spriteDef->numAnimation; loop++)
+	{
+		TileSet* tileset = spriteDef->animations[loop]->frames[0]->tileset;
+
+		VDP_loadTileSet(tileset, g_vdpTileIndex, DMA);
+
+		g_vdpTileIndex += numTiles;
+	}
+}
 
 /*
 dl_u16 g_oamIndex = 0;
@@ -468,9 +527,37 @@ void drawCustomTitleScreenBackground(GameData* gameData, const Resources* resour
 				   resources,
 				   framebuffer);
 }
+*/
+
+void drawTileText(const dl_u8* text, dl_u16 xyLocation)
+{
+    dl_u16 tilex = ((xyLocation % 32) * 8) / 8;
+    dl_u16 tiley = (xyLocation / 32) / 8;
+
+    // for each character
+    while (*text != 0xff)
+    {
+		//drawSprite(x, y, *text, font);
+		VDP_setTileMapXY(BG_A, characterFontSprite.vdpTileIndex + *text, tilex, tiley);
+
+        text++;
+        tilex++;
+    }
+}
+
+void custom_chamber_draw(dl_u8 roomNumber, GameData* gameData, const Resources* resources)
+{
+	VDP_setTileMapDataRect(BG_A, roomTileMaps[roomNumber], 0, 0, TILE_MAP_WIDTH, TILE_MAP_HEIGHT, TILE_MAP_WIDTH, DMA);
+
+	// init background and text
+	g_originalRoomDrawFunctions[roomNumber](roomNumber, gameData, resources);
+}
 
 void custom_titleScreen_draw(dl_u8 roomNumber, GameData* gameData, const Resources* resources)
 {
+	VDP_setTileMapDataRect(BG_A, roomTileMaps[roomNumber], 0, 0, TILE_MAP_WIDTH, TILE_MAP_HEIGHT, TILE_MAP_WIDTH, DMA);
+
+	/*
 	drawCustomTitleScreenBackground(gameData, resources);
 
 	dl_u8* framebuffer = gameData->cleanBackground;
@@ -489,20 +576,57 @@ void custom_titleScreen_draw(dl_u8 roomNumber, GameData* gameData, const Resourc
 	drawText(resources->text_highScore, resources->characterFont, framebuffer, 0x118b - 5 - (0x20 * 8)); // 0x158B original coco mem location
 	drawText(resources->text_playerOne, resources->characterFont, framebuffer, 0x1406 - (0x20 * 17)); // 0x1806 original coco mem location
 	drawText(resources->text_playerTwo, resources->characterFont, framebuffer, 0x1546 - (0x20 * 16)); // 0x1946 original coco mem location
+	*/
+
+	// init background and text
+//	drawBackground(&resources->roomResources[roomNumber].backgroundDrawData, 
+//				   resources,
+//				   gameData->cleanBackground);
+
+	g_originalRoomDrawFunctions[roomNumber](roomNumber, gameData, resources);
+
+	// title screen text
+	drawTileText(resources->text_downland, 0x03c9); // 0x07c9 original coco mem location
+	drawTileText(resources->text_writtenBy, 0x050a); // 0x090A original coco mem location
+	drawTileText(resources->text_michaelAichlmayer, 0x647); // 0x0A47 original coco mem location
+	drawTileText(resources->text_copyright1983, 0x789); // 0x0B89 original coco mem location
+	drawTileText(resources->text_spectralAssociates, 0x8c6); // 0x0CC6 original coco mem location
+	drawTileText(resources->text_licensedTo, 0xa0a); // 0x0E0A original coco mem location
+	drawTileText(resources->text_tandyCorporation, 0xb47); // 0x0F47 original coco mem location
+	drawTileText(resources->text_allRightsReserved, 0xc86); // 0x1086 original coco mem location
+	drawTileText(resources->text_onePlayer, 0xf05); // 0x1305 original coco mem location
+	drawTileText(resources->text_twoPlayer, 0xf11); // 0x1311 original coco mem location
+	drawTileText(resources->text_highScore, 0x118b); // 0x158B original coco mem location
+	drawTileText(resources->text_playerOne, 0x1406); // 0x1806 original coco mem location
+	drawTileText(resources->text_playerTwo, 0x1546); // 0x1946 original coco mem location
+
+	convertScoreToString(gameData->playerData[PLAYER_ONE].score, gameData->playerData[PLAYER_ONE].scoreString);
+	drawTileText(gameData->playerData[PLAYER_ONE].scoreString, TITLESCREEN_PLAYERONE_SCORE_LOCATION);
+
+	convertScoreToString(gameData->playerData[PLAYER_TWO].score, gameData->playerData[PLAYER_TWO].scoreString);
+	drawTileText(gameData->playerData[PLAYER_TWO].scoreString, TITLESCREEN_PLAYERTWO_SCORE_LOCATION);
+
+	if (gameData->playerData[PLAYER_ONE].score > gameData->highScore)
+		gameData->highScore = gameData->playerData[PLAYER_ONE].score;
+	else if (gameData->playerData[PLAYER_TWO].score > gameData->highScore)
+		gameData->highScore = gameData->playerData[PLAYER_TWO].score;
+
+	convertScoreToString(gameData->highScore, gameData->string_highScore);
+
+	drawTileText(gameData->string_highScore, TITLESCREEN_HIGHSCORE_LOCATION);
 }
 
 void custom_get_ready_room_draw(dl_u8 roomNumber, GameData* gameData, const Resources* resources)
 {
-	drawCustomTitleScreenBackground(gameData, resources);
+	VDP_setTileMapDataRect(BG_A, roomTileMaps[TITLESCREEN_ROOM_INDEX], 0, 0, TILE_MAP_WIDTH, TILE_MAP_HEIGHT, TILE_MAP_WIDTH, DMA);
 
-	dl_u8* framebuffer = gameData->cleanBackground;
+	// init background and text
+	g_originalRoomDrawFunctions[roomNumber](roomNumber, gameData, resources);
 
 	// get ready text
 	const dl_u8* getReadyString = gameData->currentPlayerData->playerNumber == PLAYER_ONE ? resources->text_getReadyPlayerOne : resources->text_getReadyPlayerTwo;
-
-	drawText(getReadyString, resources->characterFont, framebuffer, 0x0b66);
+	drawTileText(getReadyString, 0x0b66);
 }
-*/
 
 void GameRunner_Init(GameData* gameData, const Resources* resources)
 {
@@ -537,35 +661,43 @@ void GameRunner_Init(GameData* gameData, const Resources* resources)
 	g_playerIconSpriteAttributes.attr2 = OBJ_PRIORITY(UI_SPRITE_PRIORITY);
 
 	dl_u32 cursorSpriteRaw = 0xffffffff;
+	*/
 
-	// load tile resources
-	dl_u16 tileIndex = 0;
-	tileIndex = buildSpriteResource(&dropsSprite, &g_8x8SpriteAttributes, resources->sprites_drops, DROP_SPRITE_WIDTH, DROP_SPRITE_ROWS, DROP_SPRITE_COUNT, tileIndex);
-	tileIndex = buildSpriteResource(&playerSprite, &g_16x16SpriteAttributes, resources->sprites_player, PLAYER_SPRITE_WIDTH, PLAYER_SPRITE_ROWS, PLAYER_SPRITE_COUNT, tileIndex);
-	tileIndex = buildSpriteResource(&cursorSprite, &g_8x8SpriteAttributes, (dl_u8*)&cursorSpriteRaw, 8, 1, 1, tileIndex);
-	tileIndex = buildSpriteResource(&ballSprite, &g_16x8SpriteAttributes, resources->sprites_bouncyBall, BALL_SPRITE_WIDTH, BALL_SPRITE_ROWS, BALL_SPRITE_COUNT, tileIndex);
-	tileIndex = buildSpriteResource(&birdSprite, &g_16x8SpriteAttributes, resources->sprites_bird, BIRD_SPRITE_WIDTH, BIRD_SPRITE_ROWS, BIRD_SPRITE_COUNT, tileIndex);
-	tileIndex = buildSpriteResource(&keySprite, &g_16x16SpriteAttributes, resources->sprite_key, PICKUPS_NUM_SPRITE_WIDTH, PICKUPS_NUM_SPRITE_ROWS, 1, tileIndex);
-	tileIndex = buildSpriteResource(&diamondSprite, &g_16x16SpriteAttributes, resources->sprite_diamond, PICKUPS_NUM_SPRITE_WIDTH, PICKUPS_NUM_SPRITE_ROWS, 1, tileIndex);
-	tileIndex = buildSpriteResource(&moneyBagSprite, &g_16x16SpriteAttributes, resources->sprite_moneyBag, PICKUPS_NUM_SPRITE_WIDTH, PICKUPS_NUM_SPRITE_ROWS, 1, tileIndex);
-	tileIndex = buildSpriteResource(&doorSprite, &g_16x16SpriteAttributes, resources->sprite_door, DOOR_SPRITE_WIDTH, DOOR_SPRITE_ROWS, 1, tileIndex);
-	tileIndex = buildEmptySpriteResource(&regenSprite, &g_16x16SpriteAttributes, PLAYER_SPRITE_WIDTH, PLAYER_SPRITE_ROWS, 1, tileIndex);
-	tileIndex = buildTextResource(&hudCharacterFont, &g_hudTextSpriteAttributes, resources->characterFont, 8, 7, 39, tileIndex, TRUE);
-	tileIndex = buildTextResource(&characterFont, &g_textSpriteAttributes, resources->characterFont, 8, 7, 39, tileIndex, FALSE);
-	tileIndex = buildPlayerIconResource(&playerIconSprite, &g_playerIconSpriteAttributes, resources->sprites_player, PLAYER_SPRITE_WIDTH, PLAYER_SPRITE_ROWS, PLAYERICON_NUM_SPRITE_ROWS, PLAYER_SPRITE_COUNT, tileIndex);
+	// load background
+	VDP_loadTileSet(&backgroundTileset, 0, DMA);
+	g_vdpTileIndex = backgroundTileset.numTile;
 
 
+	// load sprite tile resources
+	buildTileResource(&dropsSprite, &dropTileset, DROP_SPRITE_WIDTH, DROP_SPRITE_ROWS, DROP_SPRITE_COUNT, SPRITE_SIZE(1, 1));
+	buildSpriteResource(&playerSprite, &playerTileset, PLAYER_SPRITE_WIDTH, PLAYER_SPRITE_ROWS, PLAYER_SPRITE_COUNT, SPRITE_SIZE(2, 2));
+	//buildTileResource(&cursorSprite, &g_8x8SpriteAttributes, (dl_u8*)&cursorSpriteRaw, 8, 1, 1, tileIndex);
+	buildTileResource(&ballSprite, &ballTileset, BALL_SPRITE_WIDTH, BALL_SPRITE_ROWS, BALL_SPRITE_COUNT, SPRITE_SIZE(2, 1));
+	buildTileResource(&birdSprite, &birdTileset, BIRD_SPRITE_WIDTH, BIRD_SPRITE_ROWS, BIRD_SPRITE_COUNT, SPRITE_SIZE(2, 1));
+	buildSpriteResource(&keySprite, &keyTileset, PICKUPS_NUM_SPRITE_WIDTH, PICKUPS_NUM_SPRITE_ROWS, 1, SPRITE_SIZE(2, 2));
+	buildSpriteResource(&diamondSprite, &diamondTileset, PICKUPS_NUM_SPRITE_WIDTH, PICKUPS_NUM_SPRITE_ROWS, 1, SPRITE_SIZE(2, 2));
+	buildSpriteResource(&moneyBagSprite, &moneyBagTileset, PICKUPS_NUM_SPRITE_WIDTH, PICKUPS_NUM_SPRITE_ROWS, 1, SPRITE_SIZE(2, 2));
+	buildSpriteResource(&doorSprite, &doorTileset, DOOR_SPRITE_WIDTH, DOOR_SPRITE_ROWS, 1, SPRITE_SIZE(2, 2));
+	//tileIndex = buildEmptySpriteResource(&regenSprite, &g_16x16SpriteAttributes, PLAYER_SPRITE_WIDTH, PLAYER_SPRITE_ROWS, 1, tileIndex);	
+
+	buildTileResource(&characterFontSprite, &characterFontTileset, 8, 8, CHARACTER_FONT_COUNT, SPRITE_SIZE(1, 1));
+
+	//tileIndex = buildPlayerIconResource(&playerIconSprite, &g_playerIconSpriteAttributes, resources->sprites_player, PLAYER_SPRITE_WIDTH, PLAYER_SPRITE_ROWS, PLAYERICON_NUM_SPRITE_ROWS, PLAYER_SPRITE_COUNT, tileIndex);
+
+	/*
 	buildSplatSpriteResource(tileIndex, resources);
 
 	// create the regen sprite for player icon
 	playerIconSpriteRegen.spriteAttributes = &g_playerIconSpriteAttributes;
 	playerIconSpriteRegen.tileIndex = regenSprite.tileIndex;
 	playerIconSpriteRegen.tilesPerFrame = regenSprite.tilesPerFrame;
+	*/
 
 	g_pickUpSprites[0] = &diamondSprite;
 	g_pickUpSprites[1] = &moneyBagSprite;
 	g_pickUpSprites[2] = &keySprite;
 
+	/*
 	g_scrollX = 7;
 	g_scrollY = 13;
 	*/
@@ -586,14 +718,38 @@ void GameRunner_Init(GameData* gameData, const Resources* resources)
     m_drawRoomFunctions[WIPE_TRANSITION_ROOM_INDEX] = drawWipeTransition;
     m_drawRoomFunctions[GET_READY_ROOM_INDEX] = drawGetReadyScreen;
 
-	/*
-	buildUI();
+	//buildUI();
 
+
+	g_originalRoomDrawFunctions[0] = g_rooms[0]->draw;
+	g_originalRoomDrawFunctions[1] = g_rooms[1]->draw;
+	g_originalRoomDrawFunctions[2] = g_rooms[2]->draw;
+	g_originalRoomDrawFunctions[3] = g_rooms[3]->draw;
+	g_originalRoomDrawFunctions[4] = g_rooms[4]->draw;
+	g_originalRoomDrawFunctions[5] = g_rooms[5]->draw;
+	g_originalRoomDrawFunctions[6] = g_rooms[6]->draw;
+	g_originalRoomDrawFunctions[7] = g_rooms[7]->draw;
+	g_originalRoomDrawFunctions[8] = g_rooms[8]->draw;
+	g_originalRoomDrawFunctions[9] = g_rooms[9]->draw;
+	g_originalRoomDrawFunctions[TITLESCREEN_ROOM_INDEX] = g_rooms[TITLESCREEN_ROOM_INDEX]->draw;
+	g_originalRoomDrawFunctions[GET_READY_ROOM_INDEX] = g_rooms[GET_READY_ROOM_INDEX]->draw;
+
+
+    g_rooms[0]->draw = custom_chamber_draw;
+    g_rooms[1]->draw = custom_chamber_draw;
+    g_rooms[2]->draw = custom_chamber_draw;
+    g_rooms[3]->draw = custom_chamber_draw;
+    g_rooms[4]->draw = custom_chamber_draw;
+    g_rooms[5]->draw = custom_chamber_draw;
+    g_rooms[6]->draw = custom_chamber_draw;
+    g_rooms[7]->draw = custom_chamber_draw;
+    g_rooms[8]->draw = custom_chamber_draw;
+    g_rooms[9]->draw = custom_chamber_draw;
 	g_rooms[TITLESCREEN_ROOM_INDEX]->draw = custom_titleScreen_draw;
 	g_rooms[GET_READY_ROOM_INDEX]->draw = custom_get_ready_room_draw;
-	*/
 
-	Game_ChangedRoomCallback = GameRunner_ChangedRoomCallback;
+
+	//Game_ChangedRoomCallback = GameRunner_ChangedRoomCallback;
 
 	//setGameBackgroundTilemap(TITLESCREEN_ROOM_INDEX);
 
@@ -646,58 +802,28 @@ void GameRunner_Update(GameData* gameData, const Resources* resources)
 
 void GameRunner_Draw(GameData* gameData, const Resources* resources)
 {
-	/*
-	g_transitionCounter = TRANSITION_OFF;
-
-	g_oamIndex = 0;
-	*/
+	g_numSpritesToDraw = 0;
+	VDP_clearSprites();
 
 	m_drawRoomFunctions[gameData->currentRoom->roomNumber](gameData, resources);
 
-	/*
-	BG_OFFSET[GAME_BACKGROUND_INDEX].x = g_scrollX;
-	BG_OFFSET[GAME_BACKGROUND_INDEX].y = g_scrollY;
+	VDP_setSpriteLink(g_numSpritesToDraw - 1, 0);
+	VDP_updateSprites(g_numSpritesToDraw, DMA);
 
-	for (int i = g_oamIndex; i < 128; i++) 
-	{
-		OAM[i].attr0 = ATTR0_DISABLED;
-		OAM[i].attr1 = 0;
-		OAM[i].attr2 = 0;
-	}
-	*/
+	//KDebug_AlertNumber(g_numSpritesToDraw);
 }
 
 // draw sprite, affected by scrolling
 void drawSprite(dl_u16 x, dl_u16 y, dl_u8 frame, const GameSprite* gameSprite)
 {
-	/*
-	dl_u16 tileIndex = gameSprite->tileIndex + (character * gameSprite->tilesPerFrame);
+	dl_u16 tileIndex = gameSprite->vdpTileIndex + (frame * gameSprite->tilesPerFrame);
 
-	OAM[g_oamIndex].attr0 = gameSprite->spriteAttributes->attr0 | (y & 0xff);
-	OAM[g_oamIndex].attr1 = gameSprite->spriteAttributes->attr1 | (x & 0x1ff);
-	OAM[g_oamIndex].attr2 = gameSprite->spriteAttributes->attr2 | (tileIndex << 1);
-
-	g_oamIndex++;
-	*/
+	VDP_setSpriteFull(g_numSpritesToDraw, x, y, gameSprite->vdpSize, tileIndex, g_numSpritesToDraw + 1);
+	g_numSpritesToDraw++;
 }
-
-/*
-// draw sprite not affected by scrolling
-void drawSpriteAbs(dl_u16 x, dl_u16 y, dl_u8 character, const GameSprite* gameSprite)
-{
-	dl_u16 tileIndex = gameSprite->tileIndex + (character * gameSprite->tilesPerFrame);
-
-	OAM[g_oamIndex].attr0 = gameSprite->spriteAttributes->attr0 | (y & 0xff);
-	OAM[g_oamIndex].attr1 = gameSprite->spriteAttributes->attr1 | (x & 0x1ff);
-	OAM[g_oamIndex].attr2 = gameSprite->spriteAttributes->attr2 | (tileIndex << 1);
-
-	g_oamIndex++;
-}
-*/
 
 void drawDrops(const GameData* gameData)
 {
-	/*
     // draw drops
     const Drop* dropsRunner = gameData->dropData.drops;
 
@@ -714,7 +840,6 @@ void drawDrops(const GameData* gameData)
 
         dropsRunner++;
     }
-	*/
 }
 
 void drawUIText(const dl_u8* text, dl_u16 x, dl_u16 y, GameSprite* font)
@@ -810,18 +935,19 @@ void updateScroll(dl_u16 playerX, dl_u16 playerY)
 
 void drawChamber(struct GameData* gameData, const Resources* resources)
 {
-	/*
 	PlayerData* playerData = gameData->currentPlayerData;
 
 	dl_u16 playerX = (playerData->x >> 8) << 1;
 	dl_u16 playerY = (playerData->y >> 8);
 
-	updateScroll(playerX, playerY);
+	//updateScroll(playerX, playerY);
 
 	dl_u16 currentTimer = playerData->roomTimers[playerData->currentRoom->roomNumber];
 
+
 	drawDrops(gameData);
 
+	
     // draw ball
     if (gameData->ballData.enabled)
     {
@@ -847,30 +973,34 @@ void drawChamber(struct GameData* gameData, const Resources* resources)
 	// draw player
     switch (playerData->state)
     {
-    case PLAYER_STATE_SPLAT: 
-        drawSprite(playerX,
-                   playerY + 7,
-                   playerData->splatFrameNumber,
-				   &splatSprite);
-        break;
-    case PLAYER_STATE_REGENERATION: 
-
-        if (!gameData->paused)
-        {
-			updateRegenSprite(resources, playerData->currentSpriteNumber);
-        }
-
-        drawSprite(playerX,
-                   playerY,
-                   0,
-				   &regenSprite);
-        break;
+    //case PLAYER_STATE_SPLAT: 
+    //    drawSprite(playerX,
+    //               playerY + 7,
+    //               playerData->splatFrameNumber,
+	//			   &splatSprite);
+	//
+    //    break;
+    //case PLAYER_STATE_REGENERATION: 
+	//
+    //    if (!gameData->paused)
+    //    {
+	//		updateRegenSprite(resources, playerData->currentSpriteNumber);
+    //    }
+	//
+    //    drawSprite(playerX,
+    //               playerY,
+    //               0,
+	//			   &regenSprite);
+	//
+    //    break;
+	//
     default: 
         drawSprite(playerX,
                    playerY,
                    playerData->currentSpriteNumber,
 				   &playerSprite);
     }
+
 
 	// draw pickups
     const Pickup* pickups = playerData->gamePickups[gameData->currentRoom->roomNumber];
@@ -914,15 +1044,16 @@ void drawChamber(struct GameData* gameData, const Resources* resources)
 	}
 
     // draw text
-	drawUIText(gameData->string_timer, 24 * 8, 0, &hudCharacterFont);
-	drawUIText(playerData->scoreString, 8, 0, &hudCharacterFont); 
-
-	drawUIPlayerLives(playerData);
-	*/
+	//drawUIText(gameData->string_timer, 24 * 8, 0, &hudCharacterFont);
+	//drawUIText(playerData->scoreString, 8, 0, &hudCharacterFont); 
+	//
+	//drawUIPlayerLives(playerData);
 }
 
 void drawTitleScreen(struct GameData* gameData, const Resources* resources)
 {
+	drawDrops(gameData);
+
 	/*
 	g_scrollX = 7;
 	g_scrollY = 13;

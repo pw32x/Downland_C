48af9b1fc3aa53c012d17e7dfb448569dd062137
pw_32x <git@puffweet.com>
2025-10-02 22:22:07 -0400

    updated sounds.h/.c

--- platforms/mastersystem/source/sounds.h
+++ platforms/mastersystem/source/sounds.h
@@ -10,15 +10,15 @@ extern const unsigned char	climb_up_psg[37];
 extern const unsigned char	climb_down_psg[33];
 #define				climb_down_psg_size 33
 #define				climb_down_psg_bank 2
+extern const unsigned char	land_psg[33];
+#define				land_psg_size 33
+#define				land_psg_bank 2
 extern const unsigned char	run_psg[28];
 #define				run_psg_size 28
 #define				run_psg_bank 2
 extern const unsigned char	splat_psg[24];
 #define				splat_psg_size 24
 #define				splat_psg_bank 2
-extern const unsigned char	land_psg[23];
-#define				land_psg_size 23
-#define				land_psg_bank 2
 extern const unsigned char	pickup_psg[10];
 #define				pickup_psg_size 10
 #define				pickup_psg_bank 2

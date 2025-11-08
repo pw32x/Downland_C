fc1528292769942433f31698d5ea48e2d8b5a17f
pw_32x <git@puffweet.com>
2025-11-08 15:07:37 -0500

    added set of sound effects, conversion tools, and instructions

---
+++ platforms/nes/sound/convert_looping_effects.bat
@@ -0,0 +1,2 @@
+REM use the ft2 versions of text2data
+ft2-text2data.exe LoopingEffects.txt -asm -ntsc
---
+++ platforms/nes/sound/convert_sound_effects.bat
@@ -0,0 +1,2 @@
+REM use the ft2 version of nsf2data
+ft2-nsf2data.exe SoundEffects.nsf -asm -ntsc
